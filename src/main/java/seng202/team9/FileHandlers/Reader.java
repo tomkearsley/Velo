59e9b656689d9806cdc6dc3f7518d3b0d139f9e9
Andrew Spearman <asp79@uclive.ac.nz>
2017-08-23 17:05:02 +1200

    Working on readHotspots with Scanner function

--- src/main/java/seng202/team9/FileHandlers/Reader.java
+++ src/main/java/seng202/team9/FileHandlers/Reader.java
@@ -3,6 +3,10 @@ package seng202.team9.FileHandlers;
 import seng202.team9.Models.Hotspot;
 import java.util.ArrayList;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 /**
  * The class Reader is used to load files and data
  */
@@ -16,10 +20,27 @@ public class Reader {
   /**
    * Reads WiFi hotspots from .csv file
    * @author andrewspearman
+   * @param
+   * @return ArrayList<Hotspot> Hotspots
    */
-  public void readHotspots(String filename) {
+  public ArrayList<Hotspot> readHotspots(String filename) throws FileNotFoundException {
 
+    //Scanner method
+    Scanner scanner = new Scanner(new File(filename));
+    scanner.useDelimiter(",");
+    ArrayList<Hotspot> Hotspots = new ArrayList<Hotspot>();
+
+    /*while(scanner.hasNext()){
+      Hotspots.add();
+      System.out.print(scanner.next()+"|"); //this part needs reworking!
+    }*/
+
+    scanner.close();
+
+    return Hotspots;
   }
+
+
 
 
 
