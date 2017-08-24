840235eb02d9046e2b4991442f06d37ba3e5fcc5
Kyle Lamb <kla98@uclive.ac.nz>
2017-08-24 21:10:10 +1200

    Retailer: Expanded address attributes into parts (zip, street, city etc) Reader: Implemented readRetailers() --Adaption of readHotspots()--

--- src/main/java/seng202/team9/FileHandlers/Reader.java
+++ src/main/java/seng202/team9/FileHandlers/Reader.java
@@ -4,6 +4,7 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import seng202.team9.Models.Hotspot;
+import seng202.team9.Models.Retailer;
 import java.util.ArrayList;
 import java.io.FileNotFoundException;
 
@@ -89,6 +90,75 @@ public class Reader {
     return Hotspots;
   }
 
+  /**
+   * Reads retailers from a csv file
+   *
+   * <p>
+   *   Needs to be tested to ensure it works.
+   * </p>
+   * @param filename  the name of file to open
+   * @throws FileNotFoundException  if the file cannot be found
+   * @return ArrayList<Retailer> Retailers
+   */
+  public ArrayList<Retailer> readRetailers(String filename) throws FileNotFoundException {
+
+    // Column indexes for the appropriate value per row
+    int titleIndex = 0;
+    int addressIndex = 1;
+    int floorIndex = 2;
+    int cityIndex = 3;
+    int stateIndex = 4;
+    int zipcodeIndex = 5;
+    int blockIndex = 6;
+    int descriptionIndex = 7;
+    int description2Index = 8;
+
+    // Initialize scanner and Retailers array
+    BufferedReader br = null;
+    String line;
+    ArrayList<Retailer> Retailers = new ArrayList<Retailer>();
+
+    try {
+
+      br = new BufferedReader(new FileReader(filename));
+      while ((line = br.readLine()) != null) {
+        Retailer newRetailer = new Retailer();
+
+        // Separate by comma
+        String[] csvRetailer = line.split(",");
+
+        // Set Retailer attributes from the buffered row
+        newRetailer.setTitle(csvRetailer[titleIndex]);
+        newRetailer.setAddress(csvRetailer[addressIndex]);
+        newRetailer.setFloor(csvRetailer[floorIndex]);
+        newRetailer.setCity(csvRetailer[cityIndex]);
+        newRetailer.setState(csvRetailer[stateIndex]);
+        newRetailer.setZipcode(Integer.valueOf(csvRetailer[zipcodeIndex]));
+        newRetailer.setBlock(csvRetailer[blockIndex]);
+        newRetailer.setDescription(csvRetailer[descriptionIndex]);
+        newRetailer.setSecondaryDescription(csvRetailer[description2Index]);
+
+        //add newRetailer to Retailers array
+        Retailers.add(newRetailer);
+
+      }
+
+    } catch (FileNotFoundException e) {
+      e.printStackTrace();
+    } catch (IOException e) {
+      e.printStackTrace();
+    } finally {
+      if (br != null) {
+        try {
+          br.close();
+        } catch (IOException e) {
+          e.printStackTrace();
+        }
+      }
+    }
+
+    return Retailers;
+  }
 
 
 
