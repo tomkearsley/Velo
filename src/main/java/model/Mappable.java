a3183f84cf4f0cd98a9d1d0e9ddf71d960809c4e
Imas Neupane <ine16@uclive.ac.nz>
2017-08-27 14:39:50 +1200

    Getters and settters in the interface

--- src/main/java/model/Mappable.java
+++ src/main/java/model/Mappable.java
@@ -2,8 +2,21 @@ package model;
 
 import javax.swing.ImageIcon;
 
+/**
+ * Thinking about making this a normal class, or maybe abstract because the methods can just be defined here instead of individually
+ */
+
 public interface Mappable {
   double focusLocation[] = new double[2];
   ImageIcon Logo = new ImageIcon();
   String info = "";
+
+  double[] getLocation();
+  void setLocation(double focusLocation[]);
+
+  ImageIcon getLogo();
+  void setLogo(ImageIcon Logo);
+
+  String getInfo();
+  void setInfo(String info);
 }
