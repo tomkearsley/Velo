4dad66f732b08650bae8c4eebfefce56d4336e4b
Tyler Kennedy <tke34@uclive.ac.nz>
2017-08-29 11:29:50 +1200

    Peer review: formatted code and partially refactored POITest. This needs to be completed

--- src/main/java/model/POI.java
+++ src/main/java/model/POI.java
@@ -1,14 +1,15 @@
 package model;
 
 /**
- * Created by Thomas Kearsley on 8/22/17.
+ * Created by Thomas Kearsley on 8/22/17. Checked by Tyler Kennedy on 8/29/17.
  */
-public class POI implements Mappable{
+public class POI implements Mappable {
+
   double location[] = new double[2];
   String name = "";
   String description = "";
 
-  public POI(double location[],String name,String description) {
+  public POI(double location[], String name, String description) {
     this.location = location;
     this.name = name;
     this.description = description;
@@ -17,15 +18,16 @@ public class POI implements Mappable{
 
   /**
    * Sets the Location of the POI
-   * @param location GPS Co-ordinates of POI
    *
+   * @param location GPS Co-ordinates of POI
    */
-  public void setLocation(double location[]){
+  public void setLocation(double location[]) {
     this.location = location;
   }
 
   /**
    * Sets the description of the Public POI.
+   *
    * @param description Desired description for Public POI
    */
   public void setDescription(String description) {
@@ -35,32 +37,41 @@ public class POI implements Mappable{
 
   /**
    * Sets the Name of the POI
+   *
    * @param name Desired name for POI
    */
   public void setName(String name) {
     this.name = name;
   }
 
-
   // GETTERS
 
   /**
    * Returns location of POI object.
+   *
    * @return Location of POI Object.
    */
-  public double[] getLocation() { return this.location; }
+  public double[] getLocation() {
+    return this.location;
+  }
 
   /**
    * Returns the name of POI Object
+   *
    * @return Name of POI Object.
    */
-  public String getName() {return this.name; }
+  public String getName() {
+    return this.name;
+  }
 
   /**
    * Returns the description of POI Object
+   *
    * @return Description of POI Object
    */
-  public String getDescription() {return this.description; }
+  public String getDescription() {
+    return this.description;
+  }
 
 }
 
