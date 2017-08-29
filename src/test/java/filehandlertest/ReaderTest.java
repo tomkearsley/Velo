e3608e52ec087bab04e18a7e2d737339f02e6356
Andrew Spearman <asp79@uclive.ac.nz>
2017-08-29 23:33:07 +1200

    Reviewed Kyle's code (on accident, I was supposed to review Tyler's oops)
    Signed-off-by: Andrew Spearman <asp79@uclive.ac.nz>

--- src/test/java/filehandlertest/ReaderTest.java
+++ src/test/java/filehandlertest/ReaderTest.java
@@ -87,16 +87,21 @@ public class ReaderTest {
   public void readRetailers() throws FileNotFoundException{
     List<Retailer> Retailers = reader.readRetailers("src/test/testResources/retailers.csv");
     Retailer r1, r2, r3;
-    r1 = new Retailer("Starbucks Coffee", "3 New York Plaza", "", "New York", "NY", 10004, "8-32",
-        "Casual Eating & Takeout", "F-Coffeehouse");
-    r2 = new Retailer("New York Health & Racquet Club", "39 Whitehall Street", "", "New York", "NY",
-        10004, "8-32", "Personal and Professional Services", "P-Athletic Clubs/Fitness");
-    r3 = new Retailer("A.J. Kelly's", "6 Stone Street", "", "New York", "NY", 10004, "10-32",
-        "Full Service Dining", "F-Irish Pub");
+    r1 = new Retailer("Starbucks Coffee", "3 New York Plaza", "", "New York",
+        "NY", 10004, "8-32", "Casual Eating & Takeout",
+        "F-Coffeehouse");
+    r2 = new Retailer("New York Health & Racquet Club", "39 Whitehall Street", "",
+        "New York", "NY", 10004, "8-32",
+        "Personal and Professional Services", "P-Athletic Clubs/Fitness");
+    r3 = new Retailer("A.J. Kelly's", "6 Stone Street", "", "New York",
+        "NY", 10004, "10-32", "Full Service Dining",
+        "F-Irish Pub");
+
     List<Retailer> actualRetailers = Arrays.asList(r1, r2, r3);
     for (int i = 0; i < Retailers.size(); i++) {
       assertTrue(Retailers.get(i).equals(actualRetailers.get(i)));
     }
+
   }
 
 }
