1c504a3734414eda38924f16a8787da712213c52
Tyler Kennedy <tke34@uclive.ac.nz>
2017-08-29 16:16:32 +1200

    Semi-functional GUI with a few placeholder buttons and an image where the map viewer should go

--- src/main/java/controller/GUIManager.java
+++ src/main/java/controller/GUIManager.java
@@ -1,11 +1,49 @@
 package controller;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.SplitPane;
+import javafx.scene.control.TableView;
+import javafx.scene.layout.Pane;
+import javafx.scene.layout.StackPane;
+
 /**
- * Created by andrewspearman on 8/22/17.
+ * Created by Tyler Kennedy on 29/8/17
  * 
  */
-//import javafx.fxml.FXML;
 
 public class GUIManager {
+  //menu button declarations
+  @FXML
+  private Button dataViewButton;
+  @FXML
+  private Button mapViewButton;
+  @FXML
+  private Button userViewButton;
+  @FXML
+  private Button importExportButton;
+  //Data table
+  @FXML
+  private TableView rawDataTable;
 
+
+  //stackpane which the main views sit on
+  @FXML
+  private StackPane mainStackPane;
+
+  //splitpane the TableView and buttons sit on. Children of the stackpane above.
+  @FXML
+  private SplitPane dataSplitPane;
+  @FXML
+  private Pane mapViewPane;
+
+  /*
+  Action handlers
+   */
+  public void viewData() {
+    dataSplitPane.toFront();
+  }
+  public void viewMap() {
+    mapViewPane.toFront();
+  }
 }
