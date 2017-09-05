6a16ea04a77cba7131efb51b274ec1c49a31b7ec
Tyler Kennedy <tke34@uclive.ac.nz>
2017-09-05 18:17:23 +1200

    Updates to GUIManager, readers almost implemented. commented out as is causing errors

--- src/main/java/controller/GUIManager.java
+++ src/main/java/controller/GUIManager.java
@@ -1,23 +1,67 @@
 package controller;
 
+import filehandler.Reader;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.SplitPane;
+import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.StackPane;
 import model.Hotspot;
 import model.Mappable;
+import model.POI;
+import model.PublicPOI;
+import model.Retailer;
+import model.Route;
+import model.UserPOI;
 
 /**
  * Created by Tyler Kennedy on 29/8/17
- * TODO add temporary storage (reader, arrayLists
- * TODO conversion to ObservableList for tableView
+ *
  * TODO change bottom bar on tableView to radio buttons for different tables and a "GO" button to link here
  */
 
 public class GUIManager {
+  Reader rdr = new Reader();
+
+  ArrayList<Hotspot> hotspots = new ArrayList<Hotspot>();
+  ArrayList<Retailer> retailers = new ArrayList<Retailer>();
+  ArrayList<UserPOI> userPOIs = new ArrayList<UserPOI>();
+  ArrayList<PublicPOI> publicPOIs = new ArrayList<PublicPOI>();
+  ArrayList<Route> routes = new ArrayList<Route>();
+  //TODO use reader to populate these ArrayLists
+
+  private boolean populateArrayLists() {
+    Reader rdr = new Reader();
+    try{
+      //hotspots = rdr.readHotspots("src/main/resources/InitialHotspots");
+      retailers = rdr.readRetailers("src/main/resources/Lower_Manhattan_Retailers.csv");
+
+    }
+    catch(FileNotFoundException e){
+      System.out.println("File not found");
+      e.printStackTrace();
+      //TODO bring up warning window when that is implemented
+    }
+    return true;
+  }
+
+
+  /**
+   * Runs at startup
+   * Populates the model structure with data from .csv files
+   * TODO adapt to using database primarily with csv as fallback
+   */
+  public void initialize() {
+    //populateArrayLists(); //TODO fix reader so it doesn't explode
+  }
+
   //menu button declarations
   @FXML
   private Button dataViewButton;
@@ -31,6 +75,8 @@ public class GUIManager {
   @FXML
   private TableView rawDataTable;
 
+  @FXML
+  private SplitPane mainSplitPane; //background "holder" of everything
 
   //stackpane which the main views sit on
   @FXML
@@ -54,5 +100,18 @@ public class GUIManager {
   public void viewMap() {
     mapViewPane.toFront();
   }
-  //public void dataViewHotspots() {rawDataTable.setItems(hotspotsObservable);}
+  public void dataViewHotspots() {
+    ObservableList<Hotspot> oListHotspots = FXCollections.observableArrayList(hotspots);
+    TableColumn<Hotspot, String> firstCol = new TableColumn<Hotspot, String>("Name");
+    rawDataTable.getColumns().setAll(firstCol); //something something
+    rawDataTable.setItems(oListHotspots);
+  }
+
+  public void dataViewRetailers() {
+    ObservableList<Retailer>  oListRetailers = FXCollections.observableArrayList(retailers);
+    TableColumn<Retailer, String> firstCol = new TableColumn<Retailer, String>("Name");
+    firstCol.setCellValueFactory(new PropertyValueFactory<Retailer, String>("name"));
+    rawDataTable.getColumns().setAll(firstCol);
+    rawDataTable.setItems(oListRetailers);
+  }
 }
