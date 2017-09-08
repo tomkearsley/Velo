e98c2938ad2796ba5b3d25cd44ee5b62de9e92a6
Tyler Kennedy <tke34@uclive.ac.nz>
2017-09-08 18:26:13 +1200

    updated the GUI to have a choicebox to select types of data to be shown, added a datatype enum

---
+++ src/main/java/controller/DataType.java
@@ -0,0 +1,19 @@
+package controller;
+
+public enum DataType {
+  HOTSPOT("Hotspot"),
+  RETAILER("Retailer"),
+  PUBLICPOI("Public POI"),
+  USERPOI("User POI"),
+  STATION("Station"),
+  ROUTE("Route");
+  private String type;
+
+  DataType(String type) {
+    this.type = type;
+  }
+
+  public String Type() {
+    return type;
+  }
+}
