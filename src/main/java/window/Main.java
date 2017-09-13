446f47853a1807cfcb158b277df906d307570515
Andrew Spearman <asp79@uclive.ac.nz>
2017-09-13 15:37:13 +1200

    Added Login GUI window
    Signed-off-by: Andrew Spearman <asp79@uclive.ac.nz>

--- src/main/java/window/Main.java
+++ src/main/java/window/Main.java
@@ -1,8 +1,9 @@
 package window;
 
 /**
- * Created by tyler kennedy on 25/8/17
+ * The class Main runs the main.fxml file to build the Main GUI window
  */
+
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
@@ -10,6 +11,7 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 public class Main extends Application {
+
   public static void main(String[] args) {
     launch(args);
   }
@@ -18,11 +20,12 @@ public class Main extends Application {
   public void start(Stage primaryStage) throws Exception {
     try {
       Parent root = FXMLLoader.load(getClass().getResource("/main.fxml"));//shows as an error in intelliJ but it works
-      primaryStage.setTitle("Velo");
+      primaryStage.setTitle("Vélo");
       primaryStage.setScene(new Scene(root, 500, 400));
       primaryStage.show();
     } catch (Exception e) {
       e.printStackTrace();
     }
   }
+
 }
