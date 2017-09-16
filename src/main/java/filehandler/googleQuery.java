f2502b01e6603b8e9c5e16edc4b07e8933c900c1
Imas Neupane <imas68@outlook.com>
2017-09-16 13:31:44 +1200

    Makers added on click

--- src/main/java/filehandler/googleQuery.java
+++ src/main/java/filehandler/googleQuery.java
@@ -1,18 +1,29 @@
 package filehandler;
 
 import com.google.api.client.json.Json;
+import java.io.BufferedReader;
 import java.io.InputStream;
+import javax.print.DocFlavor.URL;
 
 public class googleQuery {
 
-  String apiKey = "AIzaSyAnsKL3XnguaCwUM9kICe223bxI2KAoQkM";
+  //String apiKey = "AIzaSyAnsKL3XnguaCwUM9kICe223bxI2KAoQkM";
 
-  public String stringToJsonString(String string) {
+  public Json readURL(String string) {
+    String apiKey = "AIzaSyAnsKL3XnguaCwUM9kICe223bxI2KAoQkM";
+    String JsonString = "https://maps.googleapis.com/maps/api/geocode/json?address=" + string.replaceAll(" ","+") + "&key=" + apiKey;
     InputStream input = null;
-    return "https://maps.googleapis.com/maps/api/geocode/json?address=" + string.replaceAll(" ","+") + "&key=" + apiKey;
+    try{
+      URL url = new URL(JsonString);
+      //BufferedReader reader = new BufferedReader(url);
+    }
+    finally {
+
+    }
+    return null;
   }
 
   public static void main(String args[]) {
-
+    //System.out.println(readURL("Upper Manhattan"));
   }
 }
