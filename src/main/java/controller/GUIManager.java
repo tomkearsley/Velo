a8262d6f370493eaccb7480a2b774fceb30e5a6e
Tyler Kennedy <tke34@uclive.ac.nz>
2017-09-17 19:44:54 +1200

    partial filteredlist implementation

--- src/main/java/controller/GUIManager.java
+++ src/main/java/controller/GUIManager.java
@@ -6,11 +6,13 @@ import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.collections.transformation.FilteredList;
 import javafx.fxml.FXML;
 import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.SplitPane;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.layout.Pane;
 import javafx.scene.web.WebView;
@@ -45,6 +47,9 @@ public class GUIManager {
   private Pane mapViewPane;
   @FXML
   private ChoiceBox<DataType> dataTypeChoiceBox;
+
+  @FXML
+  private TextField rawDataFilterField;
 
   @FXML
   private WebView mapWebView;
@@ -99,6 +104,8 @@ public class GUIManager {
   public void dataViewRetailers() {
     //converting the arraylist to an observable list
     ObservableList<Retailer>  oListRetailers = FXCollections.observableArrayList(retailers);
+    FilteredList<Retailer> fListRetailers = new FilteredList<Retailer>(oListRetailers);
+    //TODO add filteredLists to other dataView__ methods, probably abstract these up a layer
     //each 2 line section creates one table heading and set of values
     //TODO lat and long from address?
     TableColumn<Retailer, String> nameCol = new TableColumn<Retailer, String>("Name");//title to be written above column
@@ -119,7 +126,7 @@ public class GUIManager {
     secondaryDescCol.setCellValueFactory(new PropertyValueFactory<Retailer, String>("secondaryDescription"));
 
     rawDataTable.getColumns().setAll(nameCol, addressCol, floorCol, cityCol, zipcodeCol, stateCol, blockCol, secondaryDescCol);
-    rawDataTable.setItems(oListRetailers);
+    rawDataTable.setItems(fListRetailers);
   }
   public void dataViewHotspots() {
     ObservableList<Hotspot> oListHotspots = FXCollections.observableArrayList(hotspots);
