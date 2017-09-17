3a2ccec1e8c1cb08a8f20dbed5af5bbe46d554df
Thomas Kearsley <tke29@uclive.ac.nz>
2017-09-17 19:28:02 +1200

    Updated Names on POI Tests

--- src/test/java/modeltest/POITest.java
+++ src/test/java/modeltest/POITest.java
@@ -12,11 +12,11 @@ import static org.junit.Assert.assertEquals;
 
 public class POITest {
 
-  private double[] l1;
-  private POI p1;
-  private double[] l2;
+  private double[] testLocation1;
+  private POI testPOI;
+  private double[] testLocation2;
   private String empireString;
-  private PublicPOI pp1;
+  private PublicPOI testPublicPOI;
   private double[] l3;
   private String userHouse;
   private UserPOI up1;
@@ -30,15 +30,12 @@ public class POITest {
     double latitude1 = -73.908305;
     double longitude2 = 40.748652;
     double latitude2 = -73.985773;
-    double longitude3;
-    double latitude3;
-    l1 = new double[]{40.719011, -73.908305};
-    p1 = new POI(longitude1,latitude1, "New York Center", "");
-    l2 = new double[]{40.748652, -73.985773};
+    testLocation1 = new double[]{40.719011, -73.908305};
+    testPOI = new POI(longitude1,latitude1, "New York Center", "");
+    testLocation2 = new double[]{40.748652, -73.985773};
     empireString = "Iconic, art deco office tower from 1931 with exhibits "
         + "& observatories on the 86th & 102nd floors.";
-    pp1 = new PublicPOI(longitude2,latitude2, "Empire State Building", empireString);
-    l3 = new double[]{};
+    testPublicPOI = new PublicPOI(longitude2,latitude2, "Empire State Building", empireString);
     userHouse = "My House";
     //up1 = new UserPOI(l3, "My House", userHouse);
     testArray1 = new double[]{40.719011, -73.908305};
@@ -52,10 +49,10 @@ public class POITest {
    */
   @Test
   public void LocationTest() {
-    double[] p1Location = {p1.getLongitude(),p1.getLatidude()};
-    double[] pp1Location = {pp1.getLongitude(),pp1.getLatidude()};
+    double[] p1Location = {testPOI.getLongitude(),testPOI.getLatidude()};
+    double[] testPublicPOILocation = {testPublicPOI.getLongitude(),testPublicPOI.getLatidude()};
     assertArrayEquals(testArray1,p1Location , delta);
-    assertArrayEquals(testArray2, pp1Location, delta);
+    assertArrayEquals(testArray2, testPublicPOILocation, delta);
   }
 
   /**
@@ -63,8 +60,8 @@ public class POITest {
    */
   @Test
   public void NameTest() {
-    assertEquals("New York Center", p1.getName());
-    assertEquals("Empire State Building", pp1.getName());
+    assertEquals("New York Center", testPOI.getName());
+    assertEquals("Empire State Building", testPublicPOI.getName());
     assertEquals("My House", up1.getName());
   }
 
@@ -81,6 +78,6 @@ public class POITest {
    */
   @Test
   public void DescriptionTestPublic() {
-    assertEquals(empireString, pp1.getDescription());
+    assertEquals(empireString, testPublicPOI.getDescription());
   }
 }
