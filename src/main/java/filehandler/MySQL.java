b1737e9e169e9264d255fb0f1a096dd80f204072
Thomas Kearsley <tke29@uclive.ac.nz>
2017-09-22 10:46:32 +1200

    Added Retrieve Queries for                          - PublicPOI

--- src/main/java/filehandler/MySQL.java
+++ src/main/java/filehandler/MySQL.java
@@ -4,6 +4,8 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.lang.Exception;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
 
 /**
  * The class MySQL defines the type which queries the MySQL Database
@@ -13,6 +15,7 @@ public class MySQL {
 
 
 
+  // DATA INSERTING
   public static void PublicPOIInsert(double longitude,double latitude,String name,String description) throws Exception{
     try {
       Connection conn = getConnection();
@@ -48,6 +51,34 @@ public class MySQL {
     }
   }
 
+
+  // DATA RETRIEVAL
+
+  public static ArrayList<Double> getPublicPOILocation(String name) throws Exception{
+    try {
+      Connection conn = getConnection();
+      PreparedStatement statement = conn.prepareStatement("SELECT longitude,latitude FROM PublicPOI");
+
+      ResultSet result = statement.executeQuery();
+
+      ArrayList<Double> location = new ArrayList<Double>();
+      while(result.next()) {
+        if (result.getString("name") == name) {
+          System.out.println(result.getDouble("longitude"));
+          System.out.println(result.getDouble("latitude"));
+
+          location.add(result.getDouble("longitude"));
+          location.add(result.getDouble("latitude"));
+          return location;
+        }
+
+      }
+
+
+    }catch (Exception e){System.out.println(e);}
+    System.out.println("Record was not found.");
+    return null;
+  }
 
   public static Connection getConnection() throws Exception{
     try{
